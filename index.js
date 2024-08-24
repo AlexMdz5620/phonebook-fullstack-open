a14cdfe8549618d022edbacca997d0112c5e21f3
Alex Mdz <ichialex5620mail.com>
2024-08-24 10:48:51 -0600

    finalizando ejercicios 3.1-3.8

--- index.js
+++ index.js
@@ -1,25 +1,101 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const morgan = require('morgan')
+const app = express();
 
-const phonebook = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        phone: "040-123456"
-      },
-      {
-          id: 2,
-        name: "Ada Lovelace",
-        phone: "39-44-5323523"
-      },
-      {
-          id: 3,
-        name: "Dan Abramov",
-        phone: "12-43-234345"
-      },
-      {
-        id: 4,
-        name: "Mary Poppendieck",
-        phone: "39-23-6423122"
-      }
-]
+app.use(express.json())
+app.use((req, res, next) => {
+  const requestBody = JSON.stringify(req.body)
+
+  morgan('tiny')(req, res, () => {
+    console.log(requestBody)
+    next()
+  })
+})
+
+let phonebook = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    phone: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    phone: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    phone: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    phone: "39-23-6423122",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+const ahora = new Date();
+app.get("/api/info", (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${phonebook.length} people</p> <br/> ${ahora}`
+  );
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(phonebook);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = phonebook.find((person) => person.id === id);
+  if (!person) {
+    res.status(404).send({ error: "Not found" });
+  }
+  res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  phonebook = phonebook.filter((person) => person.id !== id);
+
+  res.status(204).end();
+});
+
+const generateID = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  
+  if (!body.name || !body.phone) {
+    return res.status(400).json({ 
+      error: "Missing required fields"
+    });
+  } else if (phonebook.find(person => person.name === body.name)){
+    return res.status(400).json({
+      error: "Name must be unique"
+      });
+  }
+
+  const person = {
+    id: generateID(1000),
+    name: body.name,
+    phone: body.phone
+  };
+
+  phonebook = phonebook.concat(person);
+
+  res.json(person)
+});
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

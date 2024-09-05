b1d2d8ed3f82fe7506e37f9aac83352e6e3bf5ca
Alex Mdz <ichialex5620mail.com>
2024-09-05 10:04:25 -0600

    agregando cors al proyecto

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const morgan = require('morgan')
+const morgan = require('morgan');
+const cors = require('cors')
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use((req, res, next) => {
   const requestBody = JSON.stringify(req.body)
